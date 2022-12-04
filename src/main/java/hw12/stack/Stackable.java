fefe460c5afd5f0d32f667de7ef756251342316d
Yezhyck <sergeysergeevytch@gmail.com>
2022-12-04 15:44:19 +0200

    Add MyStack with exceptions. Add tests for MyStack with exceptions

--- src/main/java/hw12/stack/Stackable.java
+++ src/main/java/hw12/stack/Stackable.java
@@ -7,8 +7,14 @@ import java.util.Optional;
 
 public interface Stackable<E> {
     boolean isEmpty();
+
     boolean isFull();
+
     E push(E item) throws FullStackException;
+
     E pop() throws EmptyStackException;
+
     Optional<E> popIfPresent();
-    E peek() throws EmptyStackException;}
+
+    E peek() throws EmptyStackException;
+}
